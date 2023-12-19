fe3a2579584136b84288b2044cf9a6b4aab650b6
Chandan Mishra <imchandan1947@gmail.com>
2023-12-19 10:51:12 +0530

    Nyaysetu bug fix

--- src/Pages/Chat.jsx
+++ src/Pages/Chat.jsx
@@ -32,7 +32,7 @@ const Chat = ({ user }) => {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
-            'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+            'Authorization': `Bearer ${process.env.REACT_APP_OPEN_API_KEY}`,
           },
           body: JSON.stringify({
             model: "gpt-3.5-turbo",
